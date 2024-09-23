c2dd0e545512df6d8ef78ac227424ac810e712ee
luelD <cagaylueldawn@gmail.com>
2024-09-23 12:10:22 +0800

    inital commit

---
+++ src/it20_cagay/IT20_Cagay.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it20_cagay;
+
+/**
+ *
+ * @author Luel Dawn T. Cagay
+ */
+public class IT20_Cagay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Project Repository");
+    }
+    
+}
