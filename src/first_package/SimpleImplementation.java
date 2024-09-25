3f9a71d57516430022f99950211308f9088d04eb
luelD <cagaylueldawn@gmail.com>
2024-09-25 10:41:10 +0800

    initial commit

---
+++ src/first_package/SimpleImplementation.java
@@ -0,0 +1,14 @@
+package first_package;
+
+public class SimpleImplementation {
+    public static void main(String[] args){
+       
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Element at index 0: " + numbers[0]);
+        System.out.println("Element at index 1: " + numbers[1]);
+        System.out.println("Element at index 2: " + numbers[2]);
+        System.out.println("Element at index 3: " + numbers[3]);
+        System.out.println("Element at index 4: " + numbers[4]);
+    }
+}
+
